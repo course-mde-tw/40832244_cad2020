9af394a04ca9752356f70b554d5aa7389b22c59e
40823234 <40823234@gm.nfu.edu.tw>
2020-10-22 19:37:36 +0800

    onshape2

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'w1', 'text': '', 'tags': '', 'url': 'w1.html'}, {'title': '個人進度', 'text': '\n', 'tags': '', 'url': '個人進度.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}, {'title': 'w1', 'text': '', 'tags': '', 'url': 'w1.html'}, {'title': '個人進度', 'text': '\n \n \n \n \n \n \n 4', 'tags': '', 'url': '個人進度.html'}]};
